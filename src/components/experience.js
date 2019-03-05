c04327be7f820003280534f3d7506d76c49572f3
Christian Nguyen <cnguyen@marketo.com>
2019-03-05 13:40:40 -0800

    update tags

--- src/components/experience.js
+++ src/components/experience.js
@@ -20,13 +20,16 @@ const ExperienceListItem = styled.li`
 `;
 
 const TagItem = styled.span`
-  border: 1px solid black;
+  border: 1px solid #cfdae6;
   border-radius: 5px;
+  color: #2f363d;
+  font-weight: 500;
   margin-left: 10px;
   padding: 1px 5px;
   text-transform: uppercase;
   font-size: 12px;
-  letter-spacing
+  letter-spacing: 0.4px;
+  background-color: #cfdae6;
 `
 
 const EXPERIENCE_BANK = [
